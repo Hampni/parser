d12c5fd42434eeda3f7b3fc3a6ae8f20ecdb32e4
Hampni <magnus9044@gmail.com>
2022-08-30 18:58:59 +0300

    30.08

--- test.php
+++ test.php
@@ -5,9 +5,9 @@ $r->connect('127.0.0.1', 6379);
 
 $r->del('bad_links');
 
-//for ($i = 1; $i <= 4838; $i++) {
-//    $r->lPop('linksPage');
-//}
+for ($i = 1; $i <= 4638; $i++) {
+    $r->lPop('linksPage');
+}
 
 echo 'links: ' . $r->lLen('linksPage') . "\n";
 
